82c27920c4fda13833e286d535cac945ab75c144
John Palermo <85082089+johnpalermo@users.noreply.github.com>
2025-02-04 18:10:54 -0500

    Added character redefinition

--- base.asm
+++ base.asm
@@ -20,71 +20,22 @@ gameInit:
 
     libMMUSetBankConf(16) // RAM0, no basic rom, all ram exept for IO, kernal
     libSetVICBank(1)   // VIC Bank 1 - $4000 - $7FFF
-    libSetBorderColour(BLUE)
-    libSetBgColour0(WHITE)
+    libSetBorderColour(BLACK)
+    libSetBgColour0(BLACK)
     libFillScreen(VIC_BANK1_SCREEN,'a')
 
-//    libMMUSetBankConf(15)   
-//    jsr $CD2E       // SWAPPER - Switch to 80 column screen
+    libVDCChangeCharSet()
+    libVDCDisplayCharSet()
 
-    ldx #18         //VDC Register 18 - Update VDC address (high byte)
-    lda #$20        //High byte of Address $2000 - start of character generator data
-    jsr VDCWrite    //Write to the VDC register
-    inx             //VDC Register 19 - Update VDC address (low byte)
-    lda #$00        //Low byte of address $2000 - start of character generator data
-    jsr VDCWrite
-    ldx #31         //Data register - holds data to be stored in RAM starting at $2000, in this case.
-    ldy #0          //Index into our new character shape
-
-Char_Loop:
-    lda Character_Shape,y
-    jsr VDCWrite
-
-//    jsr VDCWrite    //A second write will advance the RAM address by 1 automatically by the VDC, thus making the char 2x the size
-    iny             //Next byte; ie next row of character
-    cpy #8          //Stop at 8 bytes
-    bcc Char_Loop
-
-Display:
-    ldx #18
-    lda #$00
-    jsr VDCWrite
-    ldx #19
-    lda #$00
-    jsr VDCWrite
-    ldx #31
-    lda #$00
-    jsr VDCWrite
-Fill80:
-    adc #$01   
-    jsr VDCWrite
-    cmp #255
-    bne Fill80
-
-    lda #$40
+    lda #$00 // @ character
     sta VIC_BANK1_SCREEN
 
-    libSetBorderColour(BLACK)
 
 gameLoop:
     libWaitRaster(255)
     jmp gameLoop
 
-//Define a character pattern (e.g., a small ship)
-Character_Shape:
-    .byte %00111100  //   ####
-    .byte %01000010  //  #    #
-    .byte %10100101  // # #  # #
-    .byte %10111101  // # #### #
-    .byte %10000001  // #      #
-    .byte %01000010  //  #    #
-    .byte %00111100  //   ####
-    .byte %00000000  // (blank row)
 
-VDCWrite:
-    stx VDC
-WaitVDC:
-    bit VDC     // M7 -> N
-    bpl WaitVDC // branch on N = 0
-    sta VDC+1
-    rts
+
+
+
--- lib/vdc.asm
+++ lib/vdc.asm
@@ -1,1 +1,49 @@
 .const VDC            = $D600
+
+.macro libVDCDisplayCharSet() {
+    Display:
+        ldx #18
+        lda #$00
+        jsr VDCWrite
+        ldx #19
+        lda #$00
+        jsr VDCWrite
+        ldx #31
+        lda #$00
+        jsr VDCWrite
+    Fill80:
+        adc #$01   
+        jsr VDCWrite
+        cmp #255
+        bne Fill80    
+}
+
+.macro libVDCChangeCharSet() {
+    ldx #18         //VDC Register 18 - Update VDC address (high byte)
+    lda #$20        //High byte of Address $2000 - start of character generator data
+    jsr VDCWrite    //Write to the VDC register
+    inx             //VDC Register 19 - Update VDC address (low byte)
+    lda #$00        //Low byte of address $2000 - start of character generator data
+    jsr VDCWrite
+    ldx #31         //Data register - holds data to be stored in RAM starting at $2000, in this case.
+    ldy #0          //Index into our new character shape
+
+Char_Loop:
+    lda Sprite_Ship,y
+    jsr VDCWrite
+
+//    jsr VDCWrite    //A second write will advance the RAM address by 1 automatically by the VDC, thus making the char 2x the size
+    iny             //Next byte; ie next row of character
+    cpy #8          //Stop at 8 bytes
+    bcc Char_Loop
+}
+
+VDCWrite:
+    stx VDC
+WaitVDC:
+    bit VDC     // M7 -> N
+    bpl WaitVDC // branch on N = 0
+    sta VDC+1
+    rts
+
+#import "sprites80.asm"
